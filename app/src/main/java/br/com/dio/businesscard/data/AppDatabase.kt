0209d6d192b1a55ed9db5129e78e00810b7c74de
Rafael Jyo Kondo <rafaeljkey@gmail.com>
2021-08-04 15:57:58 -0300

    Cartão criado.

---
+++ app/src/main/java/br/com/dio/businesscard/data/AppDatabase.kt
@@ -0,0 +1,29 @@
+package br.com.dio.businesscard.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [BusinessCard::class], version = 1)
+abstract  class AppDatabase : RoomDatabase(){
+
+    abstract fun businessDao() : BusinessCardDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase ?= null
+
+        fun getDatabase(contex: Context): AppDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    contex.applicationContext,
+                    AppDatabase::class.java,
+                    "businesscard_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
